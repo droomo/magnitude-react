08e333e951eb25f52435b9952d9b202b0c798614
moon <th@droomo.com>
2024-02-27 03:20:00 +0800

    尝试不同材质

--- src/apps/st/Scene.tsx
+++ src/apps/st/Scene.tsx
@@ -1,7 +1,7 @@
 import React, {Ref, useEffect, useMemo, useRef} from 'react';
 import {Canvas, extend, useFrame, useLoader, useThree} from '@react-three/fiber';
 import type {OrbitControls as OrbitControlsImpl} from 'three-stdlib'
-import {OrbitControls, Box, PerspectiveCamera} from '@react-three/drei';
+import {OrbitControls, Box, PerspectiveCamera, Sky} from '@react-three/drei';
 import {useTexture} from '@react-three/drei';
 import {
     BufferGeometry,
@@ -29,14 +29,15 @@ const doorWidth = 1;
 const doorHeight = 2 * doorWidth;
 
 function Ground() {
-    const [map, metalnessMap, normalMap] = useLoader(TextureLoader, [
-        `${BASE_URL}/T_Brick_Clay_Beveled_D.PNG`,
-        `${BASE_URL}/T_Brick_Clay_Beveled_M.PNG`,
-        `${BASE_URL}/T_Brick_Clay_Beveled_N.PNG`,
+    const [map, metalnessMap, normalMap, aoMap] = useLoader(TextureLoader, [
+        `${BASE_URL}/wild_grass/MI_Wild_Grass_pjwce0_4K_BaseColor.png`,
+        `${BASE_URL}/wild_grass/MI_Wild_Grass_pjwce0_4K_MetallicRoughness.png`,
+        `${BASE_URL}/wild_grass/MI_Wild_Grass_pjwce0_4K_Normal.png`,
+        `${BASE_URL}/wild_grass/MI_Wild_Grass_pjwce0_4K_Occlusion.png`,
     ]);
 
-    const repeat = new Vector2(10, 10); // 根据您的需要调整这个值
-    [map, metalnessMap, normalMap].forEach(texture => {
+    const repeat = new Vector2(4000, 4000); // 根据您的需要调整这个值
+    [map, metalnessMap, normalMap, aoMap].forEach(texture => {
         texture.wrapS = RepeatWrapping;
         texture.wrapT = RepeatWrapping;
         texture.repeat.set(repeat.x, repeat.y);
@@ -46,6 +47,7 @@ function Ground() {
         map,
         metalnessMap,
         normalMap,
+        aoMap,
         metalness: 0.4,
         roughness: 0.5,
         normalScale: new Vector2(2, 2),
@@ -53,23 +55,31 @@ function Ground() {
 
     return (
         <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} castShadow receiveShadow>
-            <planeGeometry attach="geometry" args={[50, 50]}/>
+            <planeGeometry attach="geometry" args={[1000, 1000]}/>
             <meshStandardMaterial attach="material" {...mixedTexture}/>
         </mesh>
     );
 }
 
-export default function Scene() {
+export default function Scene(props: any) {
     return (
         <Canvas>
-            <PerspectiveCamera makeDefault position={[0, 2, -10]}/>
+            <PerspectiveCamera makeDefault position={[0, doorHeight * 0.6, -10]}/>
             {/*<OrbitControls/>*/}
             <CameraController/>
-            <ambientLight/>
+            <ambientLight intensity={1}/>
             {/*<pointLight position={[0, 3, -6]} intensity={1}/>*/}
-            <pointLight position={[10, 10, 10]}/>
+            <pointLight position={[0, doorHeight, 0]} intensity={3}/>
             <Ground/>
             <Room/>
+
+            <Sky
+                distance={450000}
+                sunPosition={[5, 100, -20]}
+                inclination={0}
+                azimuth={0.25}
+                {...props}
+            />
         </Canvas>
     );
 }
@@ -110,6 +120,12 @@ function Door() {
                 if (doorGroupRef.current.rotation.y < targetRotationY) {
                     doorGroupRef.current.rotation.y = targetRotationY;
                 }
+            } else if (!open && doorGroupRef.current.rotation.y < 0) {
+                // 关门动画
+                doorGroupRef.current.rotation.y += delta * 3.2;
+                if (doorGroupRef.current.rotation.y > 0) {
+                    doorGroupRef.current.rotation.y = 0;
+                }
             }
         }
     });
@@ -136,15 +152,16 @@ function Door() {
 
 
 function Room() {
-    const [map, metalnessMap, normalMap] = useTexture([
-        `${BASE_URL}/T_CobbleStone_Smooth_D.PNG`,
-        `${BASE_URL}/T_CobbleStone_Smooth_M.PNG`,
-        `${BASE_URL}/T_CobbleStone_Smooth_N.PNG`,
+    const [map, metalnessMap, normalMap, aoMap] = useTexture([
+        `${BASE_URL}/wall/1/wall_BaseColor.png`,
+        `${BASE_URL}/wall/1/wall_MetallicRoughness.png`,
+        `${BASE_URL}/wall/1/wall_Normal.png`,
+        `${BASE_URL}/wall/1/wall_Occlusion.png`,
     ])
 
-    const repeat = new Vector2(4, 6);
+    const repeat = new Vector2(10, 10);
 
-    [map, metalnessMap, normalMap].forEach(texture => {
+    [map, metalnessMap, normalMap, aoMap].forEach(texture => {
         texture.wrapS = RepeatWrapping;
         texture.wrapT = RepeatWrapping;
         texture.repeat.set(repeat.x, repeat.y);
@@ -154,6 +171,7 @@ function Room() {
         map,
         metalnessMap,
         normalMap,
+        aoMap,
         metalness: 0.4,
         roughness: 0.5,
         normalScale: new Vector2(2, 2),
@@ -165,10 +183,10 @@ function Room() {
             <Door/>
 
             {/* 地板 */}
-            <Box args={[10 - wallThickness, wallThickness, 10 - wallThickness]}
-                 position={[0, wallThickness / 2, 0]}>
-                <meshStandardMaterial attach="material" {...mixedTexture}/>
-            </Box>
+            {/*<Box args={[10 - wallThickness, wallThickness, 10 - wallThickness]}*/}
+            {/*     position={[0, wallThickness / 2, 0]}>*/}
+            {/*    <meshStandardMaterial attach="material" {...mixedTexture}/>*/}
+            {/*</Box>*/}
             {/* 天花板 */}
             <Box args={[10 - wallThickness, wallThickness, 10 - wallThickness]}
                  position={[0, wallHeight - wallThickness / 2, 0]}>
@@ -210,20 +228,78 @@ function Room() {
 function CameraController() {
     const {camera, gl} = useThree();
     const orbitRef = useRef<OrbitControlsImpl>(null);
+    const moveSpeed = 0.05; // 调整移动速度
+
+    const [movingDirection, setMovingDirection] = React.useState<'forward' | 'backward' | 'left' | 'right' | null>(null);
+
+    const moveForward = (distance: number) => {
+        camera.translateZ(-distance);
+    }
+
+    const target = new THREE.Vector3(0, doorHeight * 0.6, 0);
+
+    const moveRight = (distance: number) => {
+        camera.translateX(distance);
+    }
 
     useFrame(() => {
         const controls = orbitRef.current;
-        // 这里可以添加逻辑来调整摄像机位置，确保它不会离开房间的边界
+
         // 示例：简单地保持摄像机的Y轴位置，防止上下移动超出预设范围
         // camera.position.clamp(new THREE.Vector3(-5, 2, -5), new THREE.Vector3(5, 4, 5));
+
+        switch (movingDirection) {
+            case 'forward':
+                moveForward(moveSpeed);
+                break;
+            case 'backward':
+                moveForward(-moveSpeed);
+                break;
+            case 'left':
+                moveRight(-moveSpeed);
+                break;
+            case 'right':
+                moveRight(moveSpeed);
+                break;
+        }
+        // camera.lookAt(target);
+        // camera.position.set(camera.position.x, doorHeight * 0.6, camera.position.z)
     });
 
-    // camera.position.set(0, 5, 10);
+    useEffect(() => {
+
+        const onKeyDown = (event: { key: any; }) => {
+            switch (event.key) {
+                case 'w': // W键向前移动
+                    setMovingDirection('forward');
+                    break;
+                case 's': // S键向后移动
+                    setMovingDirection('backward');
+                    break;
+                case 'a': // A键向左移动
+                    setMovingDirection('left');
+                    break;
+                case 'd': // D键向右移动
+                    setMovingDirection('right');
+                    break;
+                default:
+                    setMovingDirection(null);
+            }
+        };
+
+        window.addEventListener('keydown', onKeyDown);
+        window.addEventListener('keyup', () => {
+            setMovingDirection(null)
+        });
+
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [camera]);
 
     return <OrbitControls
         ref={orbitRef} args={[camera, gl.domElement]} maxPolarAngle={Math.PI / 2}
         minPolarAngle={0}
     />
 }
-
 
