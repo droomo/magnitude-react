23007e59141dd6c7427d31203aee0340d2bc2ad4
moon <th@droomo.com>
2024-03-03 23:12:32 +0800

    自适应path

--- src/apps/const.ts
+++ src/apps/const.ts
@@ -1,10 +1,12 @@
+let base_host = 'dev1.droomo.com'
+let scheme = 'https'
 
-let base_url = 'https://dev1.droomo.com'
-
-if (window.location.href.indexOf('127.0.0.1') > -1) {
-    base_url = 'http://127.0.0.1:8000'
+if (window.location.href.indexOf(base_host) === -1) {
+    scheme = 'http'
+    base_host = window.location.hostname
 }
 
 export const API = {
-    base_url: base_url,
+    base_url: `${scheme}://${base_host}:8000/`,
+    texture_base_url: `${scheme}://${base_host}:9000/`,
 }
