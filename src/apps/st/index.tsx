a5ced1ea9a634d13dd6eedf413c78bff0a4efa85
moon <th@droomo.com>
2024-03-06 06:29:22 +0800

    updated

--- src/apps/st/index.tsx
+++ src/apps/st/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import SceneShapeRadius from "./Scene/SceneShapeRadius";
 import Experiment from "./Experiment";
 import Login from "./Login";
 import './style.css'
