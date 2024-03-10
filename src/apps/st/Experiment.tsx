f38ed671f9fc84fdc185190ccdff994057774d22
moon <th@droomo.com>
2024-03-11 02:04:44 +0800

    优化实验指导语显示

--- src/apps/st/Experiment.tsx
+++ src/apps/st/Experiment.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import Trial, {TrialData} from "./Trial";
 import classes from "./css/exp.module.scss";
 import axios from "axios";
-import {API, BlockType, DEBUG, page_data} from "../const";
+import {API, DEBUG, page_data} from "../const";
 import PageMask from "./Page/PageMask";
 import {useNavigate} from "react-router-dom";
 
@@ -55,52 +55,59 @@ export default function Experiment() {
         })
     }, []);
 
-    return (!isDone && trialDataList.length) > 0 ? (breakType > 0 ? <Pause
-        done={() => {
-            if (breakType === 1) {
-                setCurrentIndex(i => i + 1)
-                setStartedIndex(i => i + 1)
-            } else {
-                navigate('/intro/');
-            }
-            setBreakType(0);
-        }}
-        time={breakType === 1 ? 60 : 60 * 3}
-        text={breakType === 1 ? '请休息1分钟' : '已经完成一组实验，请休息3分钟，之后开始下一组'}
-    /> : <Trial
-        trial={trialDataList[currentIndex]}
-        done={() => {
-            if (end_reason === 'need_change_block' && last_reaction_type_trial_id === currentIndex + 1) {
-                setBreakType(2);
-            } else if (currentIndex + 1 === trialDataList.length) {
-                if (end_reason === 'done') {
-                    setIsDone(true);
-                } else {
-                    alert('error happened 94133');
-                }
-            } else if ((currentIndex + 1) % breakTimes === 0) {
-                setBreakType(1);
-            } else {
-                setCurrentIndex(i => i + 1)
-                setStartedIndex(i => i + 1)
-            }
-        }}
-        startedIndex={startedIndex}
-    />) : <div className={classes.screen} style={{cursor: 'default'}}>
-        <div className={classes.content}>
-            <p className={classes.descriptionText}>实验已完成</p>
-            <p className={classes.descriptionTextSmall}>感谢你，{
-                localStorage.getItem('username') ? localStorage.getItem('username') + '，' : ''
-            }为心理学事业的发展做出贡献！</p>
-            <div style={{marginTop: '6rem'}}>
-                <span
-                    className={classes.fakeButton}
-                    onClick={() => {
-                        localStorage.removeItem('username');
-                        window.location.href = '/logout/';
-                    }}
-                >退出实验</span>
+    return <>
+        {
+            trialDataList.length > 0 && (breakType > 0 ? <Pause
+                done={() => {
+                    if (breakType === 1) {
+                        setCurrentIndex(i => i + 1)
+                        setStartedIndex(i => i + 1)
+                    } else {
+                        navigate('/intro/');
+                    }
+                    setBreakType(0);
+                }}
+                time={breakType === 1 ? 60 : 60 * 3}
+                text={breakType === 1 ? '请休息1分钟' : '已经完成一组实验，请休息3分钟，之后开始下一组'}
+            /> : <Trial
+                trial={trialDataList[currentIndex]}
+                done={() => {
+                    if (end_reason === 'need_change_block' && last_reaction_type_trial_id === currentIndex + 1) {
+                        setBreakType(2);
+                    } else if (currentIndex + 1 === trialDataList.length) {
+                        if (end_reason === 'done') {
+                            setIsDone(true);
+                        } else {
+                            alert('error happened 94133');
+                        }
+                    } else if ((currentIndex + 1) % breakTimes === 0) {
+                        setBreakType(1);
+                    } else {
+                        setCurrentIndex(i => i + 1)
+                        setStartedIndex(i => i + 1)
+                    }
+                }}
+                startedIndex={startedIndex}
+            />)
+        }
+        {
+            isDone && <div className={classes.screen} style={{cursor: 'default'}}>
+                <div className={classes.content}>
+                    <p className={classes.descriptionText}>实验已完成</p>
+                    <p className={classes.descriptionTextSmall}>感谢你，{
+                        localStorage.getItem('username') ? localStorage.getItem('username') + '，' : ''
+                    }为心理学事业的发展做出贡献！</p>
+                    <div style={{marginTop: '6rem'}}>
+                        <span
+                            className={classes.fakeButton}
+                            onClick={() => {
+                                localStorage.removeItem('username');
+                                window.location.href = '/logout/';
+                            }}
+                        >退出实验</span>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+        }
+    </>
 }
